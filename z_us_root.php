753833f599740d8bd92268faa642b736b8d069e4
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-07 11:26:24 -0700

    updated zroot path

--- z_us_root.php
+++ z_us_root.php
@@ -1,5 +1,5 @@
 <?php
-$path=['','users/','usersc/', 'courses/Tier2', 'courses/Tier3'];
+$path=['','users/','usersc/', 'courses/Tier2/', 'courses/Tier3/'];
 $abs_us_root=$_SERVER['DOCUMENT_ROOT'];
 
 $self_path=explode("/", $_SERVER['PHP_SELF']);
