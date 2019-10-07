9f0e45a171bd61565a90261a4d706f8168227c53
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-07 11:41:54 -0700

    updated title

--- usersc/index.php
+++ usersc/index.php
@@ -35,7 +35,8 @@ if($user->data()->first_login_pass_reset == 0) {
 	<header id="hero-section"class="jumbotron jumbotron-fluid hero">
 	  <div class="container-fluid text-center">
 	   <h3>WELCOME TO</h3>
-	   <h1 class="display-3">CPPS UNIVERSITY CERTIFICATION</h1>
+	   <h1 class="display-3">CPPS UNIVERSITY</h1>
+     <h1 class="display-3">Certification</h1>
 	   <!-- <h3 class="lead pb-4">To CPPS University</h3> -->
 	   <a href="#" id="link_about" class="btn btn-md" role="button">About Us</a>
 	   <a href="#" id="tier2_link" class="btn btn-md" role="button">Tier 2 Courses</a>
