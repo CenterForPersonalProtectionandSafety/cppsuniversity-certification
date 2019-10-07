950798ad27442cc938f0a993286a7505ff7eb1c8
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-07 11:00:48 -0700

    fixed init for correct database

--- users/init.php
+++ users/init.php
@@ -59,22 +59,22 @@ $GLOBALS['config'] = array(
 // );
 
 // Localhost
-$GLOBALS['config'] = array(
-	'mysql'      => array(
-'host'         => 'localhost',
-'username'     => 'cpps_user',
-'password'     => 'CPPSdev1!',
-'db'           => 'cpps_fbi_us5',
-),
-'remember'        => array(
-  'cookie_name'   => 'pmqesoxiw318374csb',
-  'cookie_expiry' => 604800  //One week, feel free to make it longer
-),
-'session' => array(
-  'session_name' => 'user',
-  'token_name' => 'token',
-)
-);
+// $GLOBALS['config'] = array(
+// 	'mysql'      => array(
+// 'host'         => 'localhost',
+// 'username'     => 'cpps_user',
+// 'password'     => 'CPPSdev1!',
+// 'db'           => 'cpps_fbi_us5',
+// ),
+// 'remember'        => array(
+//   'cookie_name'   => 'pmqesoxiw318374csb',
+//   'cookie_expiry' => 604800  //One week, feel free to make it longer
+// ),
+// 'session' => array(
+//   'session_name' => 'user',
+//   'token_name' => 'token',
+// )
+// );
 
 //If you changed your UserSpice or UserCake database prefix
 //put it here.
