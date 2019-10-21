e6a08e12185ec1a88d4cf41fa71b92a9efc1096a
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-21 12:13:11 -0700

    Added aboutus folder in images
    Updated image paths to about us section

--- usersc/index.php
+++ usersc/index.php
@@ -61,7 +61,7 @@ if($user->data()->first_login_pass_reset == 0) {
 	      <div class="aboutcard">
 	          <input type="checkbox" class="more" aria-hidden="true">
 	          <div class="content">
-	              <div class="front" style="background-image: url('/usersc/images/company.png')">
+	              <div class="front" style="background-image: url('/usersc/images/aboutus/company.png')">
 	                  <div class="inner">
 	                      <h4>50% of Fortune 100 corporations</h4>
 	                  </div>
@@ -71,7 +71,7 @@ if($user->data()->first_login_pass_reset == 0) {
 	      <div class="aboutcard">
 	          <input type="checkbox" class="more" aria-hidden="true">
 	          <div class="content">
-	              <div class="front" style="background-image: url('/usersc/images/university.png')">
+	              <div class="front" style="background-image: url('/usersc/images/aboutus/university.png')">
 	                  <div class="inner">
 	                      <h4>1600 colleges and universities</h4>
 	                  </div>
@@ -81,7 +81,7 @@ if($user->data()->first_login_pass_reset == 0) {
 	      <div class="aboutcard">
 	          <input type="checkbox" class="more" aria-hidden="true">
 	          <div class="content">
-	              <div class="front" style="background-image: url('/usersc/images/hospital.png')">
+	              <div class="front" style="background-image: url('/usersc/images/aboutus/hospital.png')">
 	                  <div class="inner">
 	                      <h4>2000 hospitals & many non-profit organizations</h4>
 	                  </div>
