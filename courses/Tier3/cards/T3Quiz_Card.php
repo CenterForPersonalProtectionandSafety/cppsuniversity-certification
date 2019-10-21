ed906aa9f4b25129c529dfe5340a78d995090fcb
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-21 11:55:44 -0700

    Removed temp image and added custom placeholder

--- courses/Tier3/cards/T3Quiz_Card.php
+++ courses/Tier3/cards/T3Quiz_Card.php
@@ -7,10 +7,10 @@ BL Module
     <input type="checkbox" id="card10" class="more" aria-hidden="true">
     <div class="content">
         <?php if ($user->data()->complete_t3quiz == 0){ ?>
-        <div class="front" style="background-image: url('/usersc/images/modules/quiz.png')">
+        <div class="front" style="background-image: url('/usersc/images/modules/placeholder.png')">
         <?php } ?>
         <?php if ($user->data()->complete_t3quiz == 1){ ?>
-        <div class="front" style="background-image: url('/usersc/images/modules/quiz_completed.png')">
+        <div class="front" style="background-image: url('/usersc/images/modules/placeholder.png')">
         <?php } ?>
             <div class="inner">
                 <h2>TIER 3 QUIZ</h2>
