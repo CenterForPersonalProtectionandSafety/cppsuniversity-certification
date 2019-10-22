25344f02027d6aa4f6d110c01bace3e2f7b1beba
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-22 12:27:28 -0700

    Updated T3M2 course card
    added path to new image and changed description

--- courses/Tier3/cards/T3M2_Card.php
+++ courses/Tier3/cards/T3M2_Card.php
@@ -7,13 +7,13 @@ BL Module
     <input type="checkbox" id="card8" class="more" aria-hidden="true">
     <div class="content">
         <?php if ($user->data()->complete_t3m2 == 0){ ?>
-        <div class="front" style="background-image: url('/usersc/images/modules/placeholder.png')">
+        <div class="front" style="background-image: url('/usersc/images/modules/t3m2.png')">
         <?php } ?>
         <?php if ($user->data()->complete_t3m2 == 1){ ?>
-        <div class="front" style="background-image: url('/usersc/images/modules/placeholder.png')">
+        <div class="front" style="background-image: url('/usersc/images/modules/t3m2_complete.png')">
         <?php } ?>
             <div class="inner">
-                <h2></h2>
+                <h2>Module 2</h2>
                 <label for="card8" class="button" aria-hidden="true">
                     Details
                 </label>
@@ -23,7 +23,7 @@ BL Module
             <div class="inner">
                 <div class="description">
                   <h4>Module 2</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <p>Domestic Violence</p>
                 </div>
                 <label for="card8" class="button return" aria-hidden="true">
                     <i class="fa fa-arrow-left"></i>
