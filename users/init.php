5e86c5de7f9912d68355e74b4605c4db0ed20f6a
Marnold22 <minimearnodl@gmail.com>
2020-01-05 19:38:43 -0800

    Created media queries for users-setting page.

--- users/init.php
+++ users/init.php
@@ -40,6 +40,23 @@ $GLOBALS['config'] = array(
 	)
 );
 
+// // Localhost
+// $GLOBALS['config'] = array(
+// 	'mysql'      => array(
+// 		'host'         => 'localhost',
+// 		'username'     => 'cpps',
+// 		'password'     => 'CPPSdev1!',
+// 		'db'           => 'cpps_cert_us5',
+// 		),
+// 	'remember'        => array(
+// 	  'cookie_name'   => 'pmqesoxiw318374csb',
+// 	  'cookie_expiry' => 604800  //One week, feel free to make it longer
+// 	),
+// 	'session' => array(
+// 	  'session_name' => 'user',
+// 	  'token_name' => 'token',
+// 	)
+// );
 
 //If you changed your UserSpice or UserCake database prefix
 //put it here.
